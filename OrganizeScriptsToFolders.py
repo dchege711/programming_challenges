88e4b424c6e739ad6a8c23ff5c126aa777a79f39
Chege <d.chege711@gmail.com>
2017-07-17 00:20:40 -0400

    Corrected the URLs.

--- OrganizeScriptsToFolders.py
+++ OrganizeScriptsToFolders.py
@@ -17,16 +17,18 @@ def moveIntoFolders():
 def updateReadme():
     readme = open('README.md', 'w')
 
-    # Sample URL: https://github.com/dchege711/Reddit_Daily_Programmer/tree/master/Easy/135%20%5BEasy%5D%20Arithmetic%20Equations
+    # Sample URL: https://github.com/dchege711/Reddit_Daily_Programmer/blob/master/Easy/135%20%5BEasy%5D%20Arithmetic%20Equations/135%20%5BEasy%5D%20Arithmetic%20Equations.md
+    # Generated: https://github.com/dchege711/Reddit_Daily_Programmer/            /Easy/135%20%5BEasy%5D%20Arithmetic%20Equations/135%20%5BEasy%5D%20Arithmetic%20Equations.md
+    baseURL = "https://github.com/dchege711/Reddit_Daily_Programmer/tree/master/"
     path = "/Users/dchege711/Reddit_Daily_Programmer/"
     for root, dirs, files in os.walk(path):
         for name in files:
             if name.endswith((".md")):
                 filePath = os.path.join(root, name)
-                filePath = filePath.replace('/Users/', 'https://github.com/')
+                filePath = filePath.replace('/Users/dchege711/Reddit_Daily_Programmer/', 'https://github.com/dchege711/Reddit_Daily_Programmer/blob/master/')
                 filePath = filePath.replace(' ', '%20')
                 filePath = filePath.replace('[', '%5B')
                 filePath = filePath.replace(']', '%5D')
-                readme.write("* [" + name + "](" + filePath + ")\n")
+                readme.write("* [" + name.split('.md')[0] + "](" + filePath + ")\n")
 
 updateReadme()
