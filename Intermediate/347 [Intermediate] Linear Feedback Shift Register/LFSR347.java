71889ef5b4f630fcdcbbc73180728ff86883ea16
dchege711 <d.chege711@gmail.com>
2018-01-23 03:23:07 -0500

    Solve using LFSR using ints. Debug BitSets.

--- Intermediate/347 [Intermediate] Linear Feedback Shift Register/LFSR347.java
+++ Intermediate/347 [Intermediate] Linear Feedback Shift Register/LFSR347.java
@@ -1,17 +1,26 @@
 import java.util.LinkedList;
+import java.util.BitSet;
 import java.lang.StringBuilder;
 
 public class LFSR347 {
     
-    private LinkedList<Integer> lfsr;
+    private LinkedList<BitSet> lfsr;
     private int[] tapPositions;
     
     public LFSR347(int[] initialValues, int[] tapPositions) {
-        this.lfsr = new LinkedList<Integer>();
+        this.lfsr = new LinkedList<BitSet>();
         this.tapPositions = tapPositions;
         for (int n: initialValues) {
-            this.lfsr.add(n);
+            this.lfsr.add(getBit(n));
         }
+        System.out.println(toString() + " first = " + this.lfsr.getFirst() + " last = " + this.lfsr.getLast() + "\n");
+    }
+    
+    public BitSet getBit(int a) {
+        BitSet b = new BitSet(1);
+        if (a == 1) b.set(1);
+        else if (a != 0) throw new IllegalArgumentException();
+        return b;
     }
     
     public String toString() {
@@ -22,14 +31,21 @@ public class LFSR347 {
     }
     
     public String step(String operation) {
-        int result = this.lfsr.get(this.tapPositions[0]);
+        
+        BitSet result = this.lfsr.get(this.tapPositions[0]);
         for (int i = 1; i < this.tapPositions.length; i++) {
-            result = xor(result, this.lfsr.get(this.tapPositions[i]));
-            if (operation == "XNOR") result = !result
-            elif (operation != "XOR") throw new IllegalArgumentException();
+            // System.out.print(" " + result);
+            result.xor(this.lfsr.get(this.tapPositions[i]));
+            if (operation == "XNOR") result.flip(0);
+            else if (operation != "XOR") throw new IllegalArgumentException();
+            // System.out.print(" " + operation + " " + this.lfsr.get(this.tapPositions[i]));
+            // System.out.print(" = " + result);
         }
-        this.lfsr.removeLast();
+        
+        BitSet r = this.lfsr.removeLast();
+        System.out.print(" : dropped " + r + " ... " + toString());
         this.lfsr.addFirst(result);
+        System.out.println(" added " + result + " ... " + toString());
         return toString();
     }
     
@@ -41,9 +57,9 @@ public class LFSR347 {
         int[] initialValues = {0, 0, 1};
         int[] tapPositions = {0, 2};
         LFSR347 testLFSR = new LFSR347(initialValues, tapPositions);
-        System.out.println(testLFSR.toString());
+        System.out.println("0 " + testLFSR.toString() + "\n");
         for (int i = 1; i < 8; i++) {
-            System.out.println(i + " " + testLFSR.step("XdOR"));
+            System.out.println("\n" + i + " " + testLFSR.step("XOR") + "\n");
         }
     }
     
