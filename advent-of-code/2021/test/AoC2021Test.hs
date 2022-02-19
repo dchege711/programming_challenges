3336564a3a2e5d44b76b649aff1a4602f69b9a36
Chege Gitau <d.chege711@gmail.com>
2022-02-19 12:44:25 +0300

    [AoC2021Test] Flesh out test cases
    The tests can now be run with `cabal run advent-of-code-y2021-test`. [1]
    was pretty monumental.
    
    [1]: https://github.com/hspec/HUnit

--- advent-of-code/2021/test/AoC2021Test.hs
+++ advent-of-code/2021/test/AoC2021Test.hs
@@ -1,4 +1,33 @@
 module Main (main) where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+import Data.String (IsString (fromString))
+import Paths_advent_of_code_y2021 (getDataFileName)
+import SonarSweep.SonarSweep as SonarSweep
+  ( num3MeasurementIncreases,
+    numIncreases,
+  )
+import System.IO (IOMode (ReadMode), hGetContents, withFile)
+import Test.HUnit (Counts, Test (TestCase, TestLabel, TestList), assertEqual, runTestTT)
+
+testSonarSweep :: Test
+testSonarSweep =
+  TestCase
+    ( do
+        fp <- getDataFileName "src/SonarSweep/scratchpad/sample.txt"
+        withFile
+          fp
+          ReadMode
+          ( \h -> do
+              s <- hGetContents h
+              let ls = lines (fromString s)
+              assertEqual "numIncreases," 7 (SonarSweep.numIncreases ls)
+              assertEqual "numIncreases," 5 (SonarSweep.num3MeasurementIncreases ls)
+          )
+    )
+
+tests :: Test
+tests = TestList [TestLabel "testSonarSweep" testSonarSweep]
+
+main :: IO Counts
+main = do
+  runTestTT tests
