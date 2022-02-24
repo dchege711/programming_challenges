bcefca39e0b8e9502211bf75a3760fa095d88259
Chege Gitau <d.chege711@gmail.com>
2022-02-24 17:39:03 +0300

    [AoC 21 Bin Diag] Use more robust field names for BinaryDiagnostics

--- advent-of-code/2021/src/AoC2021InputParser.hs
+++ advent-of-code/2021/src/AoC2021InputParser.hs
@@ -1,6 +1,11 @@
 {-# OPTIONS_GHC -Wall #-}
 
-module AoC2021InputParser (BinaryDiagnostics, parseBinaryDiagnosticInput) where
+-- The `(..)` syntax represents all of the constructors for the data type. [1]
+-- Without that export, we can pattern-match in BinaryDiagnostic.hs because we
+-- run into a "Not in scope: data constructor ‘BinaryDiagnostics’" error.
+--
+-- [1]: https://stackoverflow.com/a/34548070/7812406
+module AoC2021InputParser (BinaryDiagnostics(..), parseBinaryDiagnosticInput) where
 
 -- import Control.Monad.IO.Class (MonadIO)
 import Data.String (IsString (fromString))
@@ -19,14 +24,14 @@ readBin' binString = fst $ foldr f (0, 1) binString
   where
     f c (s, powerOf2) = (s + powerOf2 * digitToInt c, powerOf2 * 2)
 
--- Without the underscore prefix, I need to add `_width` and `_nums` to the
+-- Without the underscore prefix, I need to add `diagWidth` and `diagNums` to the
 -- export list to avoid `Wunused-top-binds` [1]. The field names share the top
 -- level namespace with ordinary variables and classes. [2] That's kinda
 -- inconvenient.
 --
 -- [1]: https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/using-warnings.html?highlight=unused-top-binds#ghc-flag--Wunused-top-binds
 -- [2]: https://www.haskell.org/tutorial/moretypes.html#sect6.2
-data BinaryDiagnostics = BinaryDiagnostics { _width :: Int, _nums :: [Int]}
+data BinaryDiagnostics = BinaryDiagnostics { diagWidth :: Int, diagNums :: [Int]}
 
 -- The file is a list of binary digits of the same width, e.g.
 --
@@ -42,5 +47,5 @@ parseBinaryDiagnosticInput fp = do
         s <- hGetContents h
         let ls = lines (fromString s)
         let width = maybe 0 length (listToMaybe ls)
-        return (BinaryDiagnostics{_width=width, _nums=map readBin' ls})
+        return (BinaryDiagnostics{diagWidth=width, diagNums=map readBin' ls})
     )
