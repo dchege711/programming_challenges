28819cb9e25171510275f59f722df88b7aa90a45
Chege Gitau <d.chege711@gmail.com>
2022-02-24 18:34:44 +0300

    [AoC 21 Bin Diag] Use DeepSeq to force strict I/O

--- advent-of-code/2021/src/AoC2021InputParser.hs
+++ advent-of-code/2021/src/AoC2021InputParser.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# Language RecordWildCards #-}
 
 -- The `(..)` syntax represents all of the constructors for the data type. [1]
 -- Without that export, we can pattern-match in BinaryDiagnostic.hs because we
@@ -7,7 +8,7 @@
 -- [1]: https://stackoverflow.com/a/34548070/7812406
 module AoC2021InputParser (BinaryDiagnostics(..), parseBinaryDiagnosticInput) where
 
--- import Control.Monad.IO.Class (MonadIO)
+import Control.DeepSeq (($!!), NFData, rnf)
 import Data.String (IsString (fromString))
 import Paths_advent_of_code_y2021 (getDataFileName)
 import System.IO (IOMode (ReadMode), hGetContents, withFile)
@@ -33,6 +34,14 @@ readBin' binString = fst $ foldr f (0, 1) binString
 -- [2]: https://www.haskell.org/tutorial/moretypes.html#sect6.2
 data BinaryDiagnostics = BinaryDiagnostics { diagWidth :: Int, diagNums :: [Int]}
 
+-- For the `($!!)` operator to work on `BinaryDiagnostics`, we need to be an
+-- instance of `NFData`. [1] ([2] for syntax)
+--
+-- [1]: https://hackage.haskell.org/package/deepseq-1.4.6.1/docs/Control-DeepSeq.html#t:NFData
+-- [2]: https://stackoverflow.com/a/31478918/7812406
+instance NFData BinaryDiagnostics where
+    rnf BinaryDiagnostics{ .. } = rnf diagWidth `seq` rnf diagNums
+
 -- The file is a list of binary digits of the same width, e.g.
 --
 -- 00100
@@ -47,5 +56,19 @@ parseBinaryDiagnosticInput fp = do
         s <- hGetContents h
         let ls = lines (fromString s)
         let width = maybe 0 length (listToMaybe ls)
-        return (BinaryDiagnostics{diagWidth=width, diagNums=map readBin' ls})
+        -- With lazy I/O, `h` gets closed as soon as we leave `withFile`. This
+        -- introduces errors of the form "hGetContents: illegal operation
+        -- (delayed read on closed handle)". [1]
+        --
+        -- In the expression `f $!! x`, `x` is fully evaluated before `f` is
+        -- applied to it. [1] [2]
+        --
+        -- Alternatively, one can use `readFile` which holds the file open until
+        -- it has finished reading the file [1]. However, [3] says that
+        -- `readFile` reads the file lazily. Hmm...
+        --
+        -- [1]: https://stackoverflow.com/a/26949379/7812406
+        -- [2]: https://hackage.haskell.org/package/deepseq-1.4.6.1/docs/Control-DeepSeq.html#v:-36--33--33-
+        -- [3]: https://hackage.haskell.org/package/base-4.16.0.0/docs/Prelude.html#v:readFile
+        return $!! (BinaryDiagnostics{diagWidth=width, diagNums=map readBin' ls})
     )
