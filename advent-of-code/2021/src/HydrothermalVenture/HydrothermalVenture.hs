d215b22ac57153e275ab8565885d9e4ceb319e0f
Chege Gitau <d.chege711@gmail.com>
2022-03-01 00:02:51 +0300

    [AoC 21 Hydrothermal Venture] Attempt Part II

--- advent-of-code/2021/src/HydrothermalVenture/HydrothermalVenture.hs
+++ advent-of-code/2021/src/HydrothermalVenture/HydrothermalVenture.hs
@@ -4,11 +4,11 @@ module HydrothermalVenture.HydrothermalVenture
   ( pointsWithAtLeastTwoRightSegmentOverlaps,
     pointsWithAtLeastTwoSegmentOverlaps,
     LineSegment (..),
-    Point (..)
+    Point (..),
   )
 where
 
-import Data.List (sort, group)
+import Data.List (group, sort)
 
 -- The input text is of the form:
 --
@@ -19,44 +19,69 @@ import Data.List (sort, group)
 -- segment makes sense.
 --
 -- Kinda wild that deriving `Eq` and `Ord` simply works!
-data Point = Point {x :: Int, y :: Int} deriving (Eq, Ord)
+data Point = Point {x :: Int, y :: Int} deriving (Eq, Ord, Show)
+
 data LineSegment = LineSegment {p1 :: Point, p2 :: Point}
 
-discretizeRightLineSegments :: LineSegment -> [Point]
-discretizeRightLineSegments LineSegment{ p1=Point{x=x1, y=y1}, p2=Point{x=x2, y=y2} }
-    | x1 == x2 || y1 == y2 =
-        let
-            steps :: Int -> Int -> [Int]
-            steps i1 i2 = if i2 > i1 then [i1 .. i2] else [i2 .. i1]
-            xs = steps x1 x2
-            ys = steps y1 y2
+type LineSegmentDiscretizer = LineSegment -> [Point]
 
-        in map (\p -> Point{x = head p, y = p !! 1}) (sequence[xs,ys])
+discretizeRightLineSegment :: LineSegmentDiscretizer
+discretizeRightLineSegment LineSegment {p1 = Point {x = x1, y = y1}, p2 = Point {x = x2, y = y2}}
+  | x1 == x2 || y1 == y2 =
+    let steps :: Int -> Int -> [Int]
+        steps i1 i2 = if i2 > i1 then [i1 .. i2] else [i2 .. i1]
+        xs = steps x1 x2
+        ys = steps y1 y2
+     in map (\p -> Point {x = head p, y = p !! 1}) (sequence [xs, ys])
+  | otherwise = []
 
-    | otherwise = []
+groupSegmentOverlaps :: LineSegmentDiscretizer -> [LineSegment] -> [[Point]]
+groupSegmentOverlaps discretizer lineSegments =
+  -- In an imperative language with mutable data, I'd have had a mapping from
+  -- points to counts, and then looped over the map to get those w/ counts >=
+  -- 2. But we don't have mutability...
+  --
+  -- Suppose we had a list of points with a vent, e.g. [(x1, y1), (x2, y2),
+  -- (x3, y3), (x1, y1), (x4, y4)]. How do I get counts without mutability?
+  --
+  -- Seems like some version of folding, where the accumulator is of the form
+  -- [(x1, x2, 2), (x2, y2, 1), (x3, y3, 1), (x4, y4, 1)]. Feels pretty
+  -- inefficient. Or we can sort the list, and then keeping count doesn't need
+  -- take quadratic(?) time.
+  let expandSegmentsToPoints :: [LineSegment] -> [Point]
+      expandSegmentsToPoints [] = []
+      expandSegmentsToPoints (seg : segs) =
+        discretizer seg ++ expandSegmentsToPoints segs
+   in group (sort (expandSegmentsToPoints lineSegments))
 
 pointsWithAtLeastTwoRightSegmentOverlaps :: [LineSegment] -> Int
-pointsWithAtLeastTwoRightSegmentOverlaps lineSegments =
-    -- In an imperative language with mutable data, I'd have had a mapping from
-    -- points to counts, and then looped over the map to get those w/ counts >=
-    -- 2. But we don't have mutability...
-    --
-    -- Suppose we had a list of points with a vent, e.g. [(x1, y1), (x2, y2),
-    -- (x3, y3), (x1, y1), (x4, y4)]. How do I get counts without mutability?
-    --
-    -- Seems like some version of folding, where the accumulator is of the form
-    -- [(x1, x2, 2), (x2, y2, 1), (x3, y3, 1), (x4, y4, 1)]. Feels pretty
-    -- inefficient. Or we can sort the list, and then keeping count doesn't need
-    -- take quadratic(?) time.
-    let
-        expandSegmentsToPoints :: [LineSegment] -> [Point]
-        expandSegmentsToPoints [] = []
-        expandSegmentsToPoints (seg:segs) =
-            discretizeRightLineSegments seg ++ expandSegmentsToPoints segs
+pointsWithAtLeastTwoRightSegmentOverlaps segs =
+  length
+    ( filter
+        (\ps -> length ps > 1)
+        ( groupSegmentOverlaps
+            discretizeRightLineSegment
+            segs
+        )
+    )
 
-        groupedPoints = group (sort (expandSegmentsToPoints lineSegments))
+discretizeDiagonalLineSegment :: LineSegmentDiscretizer
+discretizeDiagonalLineSegment LineSegment {p1 = Point {x = x1, y = y1}, p2 = Point {x = x2, y = y2}}
+  | abs (x1 - x2) == abs (y1 - y2) =
+    let absDiff = abs (x1 - x2)
+        xStep = (x2 - x1) `div` absDiff
+        yStep = (y2 - y1) `div` absDiff
+        xs = [x1, x1 + xStep .. x2]
+        ys = [y1, y1 + yStep .. y2]
+     in zipWith (\x' y' -> Point {x = x', y = y'}) xs ys
+  | otherwise = []
 
-    in length (filter (\ps -> length ps > 1) groupedPoints)
+discretizeLineSegments :: LineSegmentDiscretizer
+discretizeLineSegments lineSeg@LineSegment {p1 = Point {x = x1, y = y1}, p2 = Point {x = x2, y = y2}}
+  | x1 == x2 || y1 == y2 = discretizeRightLineSegment lineSeg
+  | abs (x1 - x2) == abs (y1 - y2) = discretizeDiagonalLineSegment lineSeg
+  | otherwise = []
 
 pointsWithAtLeastTwoSegmentOverlaps :: [LineSegment] -> Int
-pointsWithAtLeastTwoSegmentOverlaps _ = 50
+pointsWithAtLeastTwoSegmentOverlaps segs =
+  length (filter (\ps -> length ps > 1) (groupSegmentOverlaps discretizeLineSegments segs))
