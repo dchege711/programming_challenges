d25243e2e2f803e8b14fd5c10145933270ef66f6
Chege Gitau <d.chege711@gmail.com>
2022-03-01 15:42:01 +0300

    [AoC 21] [ES] Add comment for sed '1,2d'

--- advent-of-code/2021/publish_lhs_files.zsh
+++ advent-of-code/2021/publish_lhs_files.zsh
@@ -5,6 +5,6 @@ lhs_files=($working_dir/**/*.lhs)
 
 for lhs_file in $lhs_files ; do
     output_file=${lhs_file/.lhs/.md} # Replace .lhs with .md
-    lhs2tex $lhs_file | sed '1,2d' > $output_file
+    lhs2tex $lhs_file | sed '1,2d' > $output_file # Delete first 2 lines
     printf "Published $output_file\n"
 done
