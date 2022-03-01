e60b1d83473bd6fb226ae743d10cb904b8fcc51b
Chege Gitau <d.chege711@gmail.com>
2022-03-01 15:34:14 +0300

    [AoC 21] [ES] Fix script for publishing LHS files

--- advent-of-code/2021/publish_lhs_files.zsh
+++ advent-of-code/2021/publish_lhs_files.zsh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
 working_dir="$(dirname $0)"
-printf "Looking for .LHS files in $working_dir\n"
-
 lhs_files=($working_dir/**/*.lhs)
 
-for f in $lhs_files ; do
-    output_file=${f/.lhs/.md} # Replace .lhs with .md
-    lhs2tex --markdown src/Lanternfish/Lanternfish.lhs | sed '1,2d' > output_file
+for lhs_file in $lhs_files ; do
+    output_file=${lhs_file/.lhs/.md} # Replace .lhs with .md
+    lhs2tex $lhs_file | sed '1,2d' > $output_file
     printf "Published $output_file\n"
 done
