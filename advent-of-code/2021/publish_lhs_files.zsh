5c017d36928b27ed0d4c9779076d873a41848603
Chege Gitau <d.chege711@gmail.com>
2022-03-01 19:29:02 +0300

    [AoC 21] [ES] Replace "\#" at the beginning of lines w/ "#"

--- advent-of-code/2021/publish_lhs_files.zsh
+++ advent-of-code/2021/publish_lhs_files.zsh
@@ -5,6 +5,9 @@ lhs_files=($working_dir/**/*.lhs)
 
 for lhs_file in $lhs_files ; do
     output_file=${lhs_file/.lhs/.md} # Replace .lhs with .md
-    lhs2tex --markdown $lhs_file | sed '1,2d' > $output_file # Delete first 2 lines
+    # Delete the first 2 lines which only have a `%` character.
+    # The 2nd expression replaces `\#` with `#`. The former is needed for .lhs
+    # to compile. As an SO user said, when in doubt, add another backslash.
+    lhs2tex --markdown $lhs_file | sed -e '1,2d' -e s.^\\\\\#.\#. > $output_file
     printf "Published $output_file\n"
 done
