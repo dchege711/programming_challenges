b9bdff70a8673d99361dcc3cf95ac13c8ddb43c3
Chege Gitau <d.chege711@gmail.com>
2025-06-25 22:07:36 -0700

    [AoC 2024] dotnet hello world

---
+++ advent-of-code/2024/AoC2024/Program.cs
@@ -0,0 +1,19 @@
+﻿// Uses top-level statements. The compiler generates a `Program` class with an
+// entry point method. This code is roughly equivalent to
+//
+// ```cs
+// using System;
+// namespace AoC2024
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
+// ```
+//
+// [1]: https://aka.ms/new-console-template
+Console.WriteLine("Hello, World!");
