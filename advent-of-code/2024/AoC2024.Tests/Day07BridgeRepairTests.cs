65b8bf8eae7ed07c5e018805197633d759df0156
Chege Gitau <d.chege711@gmail.com>
2025-09-14 09:56:40 -0700

    [AoC 2024] [Bridge Repair] Debugging information

--- advent-of-code/2024/AoC2024.Tests/Day07BridgeRepairTests.cs
+++ advent-of-code/2024/AoC2024.Tests/Day07BridgeRepairTests.cs
@@ -42,4 +42,13 @@ public sealed class Day07BridgeRepairTests
         var calibrationTotal = BridgeRepair.TotalCalibrationResultWithConcat(calibrations);
         calibrationTotal.Should().Be(expectedCalibrationTotal);
     }
+
+    [TestMethod]
+    public void SingleEquation()
+    {
+        IList<BridgeRepair.CalibrationEquation> calibrations = [
+            new(7290L, [6L, 8L, 6L, 15L])];
+        var calibrationTotal = BridgeRepair.TotalCalibrationResultWithConcat(calibrations);
+        calibrationTotal.Should().Be(7291L);
+    }
 }
--- advent-of-code/2024/AoC2024/07-bridge-repair/BridgeRepair.PartOne.cs
+++ advent-of-code/2024/AoC2024/07-bridge-repair/BridgeRepair.PartOne.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using System.Text;
 
 namespace AoC2024;
 
@@ -18,27 +19,42 @@ public partial class BridgeRepair
             .Sum();
 
     private static bool IsValid(CalibrationEquation equation, ImmutableHashSet<Operator> operators) =>
-        PermutationWithReplacement(operators, [], equation.Operands.Count)
+        PermutationWithReplacement(operators, [], equation.Operands.Count - 1)
             .Any(operators => IsValid(equation, operators));
 
     private static IEnumerable<ImmutableList<Operator>> PermutationWithReplacement(
         ImmutableHashSet<Operator> seedOperators, ImmutableList<Operator> operators, int desiredLength)
     {
         if (operators.Count == desiredLength)
+        {
+            Console.WriteLine($"Yielding: Base case: {string.Join(" ", operators.Select(ToString))}");
             yield return operators;
+        }
         
         if (operators.Count > desiredLength)
+        {
+            Console.WriteLine($"Yielding: break");
             yield break;
+        }
         
         foreach (var @operator in seedOperators)
+        {
             foreach (var permutation in PermutationWithReplacement(seedOperators, operators.Add(@operator), desiredLength))
+            {
+                Console.WriteLine($"Yielding: Permutation {string.Join(" ", permutation.Select(ToString))}");
                 yield return permutation;
+            }
+        }
     }
 
     private static bool IsValid(
         CalibrationEquation equation, IEnumerable<Operator> operators)
     {
         long result = equation.Operands.First();
+
+        StringBuilder stringBuilder = new();
+        stringBuilder.Append(result);
+
         foreach (var (@operator, operand) in Enumerable.Zip(operators, equation.Operands.Skip(1)))
         {
             result = @operator switch {
@@ -47,12 +63,28 @@ public partial class BridgeRepair
                 Operator.Concatenate => Concatenate(result, operand),
                 _ => throw new ArgumentException($"Unrecognized op: {@operator}")
             };
+
+            stringBuilder.Append($" {ToString(@operator)} {operand}");
+
             if (result > equation.Result)
+            {
+                stringBuilder.Append($" (...can only increase from here)");
                 break;
+            }
         }
+
+        stringBuilder.Append($" == {result} ?= {equation.Result} : {result == equation.Result}");
+        Console.WriteLine(stringBuilder);
 
         return result == equation.Result;
     }
+
+    private static string ToString(Operator @operator) => @operator switch {
+        Operator.Add => "+",
+        Operator.Multiply => "x",
+        Operator.Concatenate => "||",
+        _ => throw new ArgumentException($"Unrecognized op: {@operator}")
+    };
 
     private enum Operator { Add, Multiply, Concatenate }
 }
