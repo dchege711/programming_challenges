e5691d599bc1efc8e5ace8dbf4b8097fc1e062a2
Chege Gitau <d.chege711@gmail.com>
2025-09-28 16:43:36 -0700

    [AoC 2024] [Disk Fragmenter] Clean up debug code

--- advent-of-code/2024/AoC2024/09-disk-fragmenter/DiskFragmenter.PartTwo.cs
+++ advent-of-code/2024/AoC2024/09-disk-fragmenter/DiskFragmenter.PartTwo.cs
@@ -2,29 +2,15 @@ namespace AoC2024;
 
 public partial class DiskFragmenter
 {
-    public static long PartTwo(IEnumerable<int> diskMap)
-    {
-        var defragmentedFiles = DefragmentFiles(ExpandDiskMap(diskMap).ToArray()).ToArray();
-        return defragmentedFiles
+    public static long PartTwo(IEnumerable<int> diskMap) =>
+        DefragmentFiles(ExpandDiskMap(diskMap).ToArray())
             .Select((id, idx) => IsFreeBlock(id) ? 0L : (long)id * idx)
             .Sum();
-    }
         
 
-    private static IEnumerable<int> DefragmentFiles(int[] diskMap)
+    private static int[] DefragmentFiles(int[] diskMap)
     {
-        /**
-        00...111...2...333.44.5555.6666.777.888899
-        0099.111...2...333.44.5555.6666.777.8888..
-        0099.1117772...333.44.5555.6666.....8888..
-        0099.111777244.333....5555.6666.....8888..
-        00992111777.44.333....5555.6666.....8888..
-        */
-
         var ri = diskMap.Length - 1;
-        while (IsFreeBlock(diskMap[ri]) && ri >= 0)
-            ri--;
-
         while (ri > 0)
         {
             var riBlock = diskMap[ri];
@@ -39,7 +25,6 @@ public partial class DiskFragmenter
             var maybeNewLocation = GetFirstAvailableFreeSpace(diskMap, ri - riBlockSize + 1, riBlockSize);
             if (maybeNewLocation is int newLocation)
             {
-                // Defragment ri's block by moving to left-most available block.
                 for (int i = 0; i < riBlockSize; i++)
                 {
                     diskMap[newLocation + i] = riBlock;
