a7c3fb538a71c9b0e7a6e356ed09232895b87a29
Chege Gitau <d.chege711@gmail.com>
2026-03-07 16:25:54 +0000

    [AoC 2024] [Warehouse Woes] Do most of the work in getting cells to move (slow code though)

--- advent-of-code/2024/AoC2024/15-warehouse-woes/WarehouseWoes.PartOne.cs
+++ advent-of-code/2024/AoC2024/15-warehouse-woes/WarehouseWoes.PartOne.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Diagnostics;
 using AoC2024.WarehouseWoesDataTypes;
 using ExhaustiveMatching;
@@ -29,109 +30,72 @@ public partial class WarehouseWoes
     {
         var delta = direction.ToDelta();
         var isLateralMove = direction.IsLateral();
-        Delta leftDelta = new(0, -1);
-        Delta rightDelta = leftDelta.Reverse();
 
-        IEnumerable<Coordinate> moveFrontier = [];
-
-        IEnumerable<Coordinate> targetCoords = [ origin + delta ];
-        while (targetCoords.All(coord => coord.IsInBounds(grid)))
-        {
-            var cellTypes = targetCoords.Select(coord => grid[coord.R, coord.C]);
-            if (cellTypes.Any(ct => ct == CellType.Wall))
-                break;
-
-            if (cellTypes.All(ct => ct == CellType.Free))
-            {
-                moveFrontier = targetCoords;
-                break;
-            }
-
-            targetCoords = targetCoords.SelectMany(coord =>
-            {
-                Coordinate[] res = grid[coord.R, coord.C] switch
-                {
-                    CellType.Box => [coord + delta],
-
-                    CellType.BoxStart => isLateralMove
-                        ? [coord + delta]
-                        : [coord + delta, coord + rightDelta + delta],
-
-                    CellType.BoxEnd => isLateralMove
-                        ? [coord + delta]
-                        : [coord + delta, coord + leftDelta + delta],
-
-                    CellType.Wall => throw new ArgumentException(
-                        "Walls should have exited the loop already"),
-
-                    CellType.Free => [],
-
-                    _ => throw ExhaustiveMatch.Failed(grid[coord.R, coord.C])
-                };
-                return res;
-            });
-        }
-
-        if (!moveFrontier.Any())
+        var cellsToShiftOver = GetCellsToShiftOver(grid, origin, direction);
+        Debug.WriteLine($"Moving: {string.Join(',', cellsToShiftOver.Reverse().Select(p => $"({p.R}, {p.C})"))}");
+        if (cellsToShiftOver.Count == 0)
             return origin;
 
-        while (moveFrontier.Any(coord => coord != origin))
+        foreach (var cell in cellsToShiftOver.Reverse())
         {
-            Debug.WriteLine($"Move frontier: {string.Join(',', moveFrontier.Select(p => $"({p.R}, {p.C})"))}");
-            HashSet<Coordinate> previousMoveFrontier = [];
-            foreach (var target in moveFrontier)
-            {
-                var source = target - delta;
-                var sourceCellType = grid[source.R, source.C];
-                switch (sourceCellType)
-                {
-                    case CellType.Box:
-                        {
-                            grid[target.R, target.C] = sourceCellType;
-                            grid[source.R, source.C] = CellType.Free;
-                            previousMoveFrontier.Add(source);
-                            break;
-                        }
-
-                    case CellType.BoxStart:
-                        {
-                            var canMove = isLateralMove || moveFrontier.Contains(target + rightDelta);
-                            if (!canMove)
-                                break;
-
-                            grid[target.R, target.C] = sourceCellType;
-                            grid[source.R, source.C] = CellType.Free;
-                            previousMoveFrontier.Add(source);
-                            break;
-                        }
-
-                    case CellType.BoxEnd:
-                        {
-                            var canMove = isLateralMove || moveFrontier.Contains(target + leftDelta);
-                            if (!canMove)
-                                break;
-
-                            grid[target.R, target.C] = sourceCellType;
-                            grid[source.R, source.C] = CellType.Free;
-                            previousMoveFrontier.Add(source);
-                            break;
-                        }
-
-                    case CellType.Free:
-                        break;
-
-                    case CellType.Wall:
-                        throw new ArgumentException("Walls should have exited the loop already");
-
-                    default:
-                        throw ExhaustiveMatch.Failed(sourceCellType);
-                };
-            }
-            grid.Visualize(origin);
-
-            moveFrontier = previousMoveFrontier;
+            var source = cell - delta;
+            grid[cell.R, cell.C] = grid[source.R, source.C];
         }
 
         return origin + delta;
     }
+
+    private static IReadOnlyList<Coordinate> GetCellsToShiftOver(CellType[,] grid, Coordinate origin, Direction direction)
+    {
+        var delta = direction.ToDelta();
+        var isLateralMove = direction.IsLateral();
+
+        IReadOnlyList<Coordinate> candidates = GetCellsAffectedByMove(grid, origin, direction);
+        var cellsToShift = ImmutableList.CreateBuilder<Coordinate>();
+        cellsToShift.AddRange(candidates);
+        while (candidates.All(coord => coord.IsInBounds(grid)))
+        {
+            var cellTypes = candidates.Select(coord => grid[coord.R, coord.C]).ToArray();
+
+            if (cellTypes.Any(ct => ct is CellType.Wall))
+                return [];
+
+            if (cellTypes.All(ct => ct is CellType.Free))
+                return cellsToShift.ToList();
+
+            candidates = candidates
+                .SelectMany(coord => GetCellsAffectedByMove(grid, coord, direction))
+                .ToList();
+            cellsToShift.AddRange(candidates);
+        }
+
+        return [];
+    }
+
+    private static IReadOnlyList<Coordinate> GetCellsAffectedByMove(
+        CellType[,] grid, Coordinate origin, Direction direction)
+    {
+        var delta = direction.ToDelta();
+        var target = origin + delta;
+
+        if (direction.IsLateral())
+            return [target];
+
+        if (!target.IsInBounds(grid))
+            return [target];
+
+        var cellType = grid[target.R, target.C];
+        return cellType switch
+        {
+            CellType.Wall => [target],
+            CellType.Box => [target],
+            CellType.Free => [target],
+            CellType.BoxStart => [target, target + RightDelta],
+            CellType.BoxEnd => [target, target + LeftDelta],
+            _ => throw ExhaustiveMatch.Failed(cellType)
+        };
+    }
+
+    private static Delta LeftDelta = new(0, -1);
+    private static Delta RightDelta = new(0, -1);
 }
