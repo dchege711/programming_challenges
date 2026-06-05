07e127226d22ee27c66c46711d97c19b7b69883f
Chege Gitau <d.chege711@gmail.com>
2026-06-04 18:21:09 -0700

    [LC] [FooBar Threading] Deadlocked and incorrect solution

--- leet-code-and-others/concurrency/print-foobar-alternately/print_foobar_alternately.py
+++ leet-code-and-others/concurrency/print-foobar-alternately/print_foobar_alternately.py
@@ -1,14 +1,29 @@
+from threading import Lock, Event
 from typing import Callable
 
 
 class FooBar:
     def __init__(self, n):
         self.n = n
+        self.lock = Lock()
+        self.foo_called = Event()
+        self.lock.acquire()
 
     def foo(self, printFoo: "Callable[[], None]") -> None:
-        for _ in range(self.n):
-            printFoo()
+        printFoo()
+        self.foo_called.set()
+
+        for _ in range(self.n - 1):
+            with self.lock:
+                printFoo()
 
     def bar(self, printBar: "Callable[[], None]") -> None:
-        for _ in range(self.n):
+        iteration_range = self.n
+        if self.foo_called.is_set():
             printBar()
+            iteration_range -= 1
+            self.lock.release()
+
+        for _ in range(iteration_range):
+            with self.lock:
+                printBar()
---
+++ leet-code-and-others/concurrency/print-foobar-alternately/test_solution.py
@@ -0,0 +1,38 @@
+from threading import Thread
+from typing import List
+
+import pytest
+
+from .print_foobar_alternately import FooBar
+
+@pytest.mark.parametrize("call_a_first", [True, False])
+def test_solution(call_a_first: bool):
+    n = 2
+    foo_bar = FooBar(n)
+
+    log: List[str] = []
+
+    def printFoo():
+        log.append("foo")
+
+    def printBar():
+        log.append("bar")
+
+    def call_foo():
+        foo_bar.foo(printFoo)
+
+    def call_bar():
+        foo_bar.bar(printBar)
+
+    thread_a = Thread(target=call_foo)
+    thread_b = Thread(target=call_bar)
+
+    threads = (thread_a, thread_b) if call_a_first else  (thread_b, thread_a)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    assert log == ["foo", "bar"] * n
